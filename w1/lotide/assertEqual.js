280864260869ba313d18606e8aeed7e50990eb7d
Scott Appleton <Scottappleton09@gmail.com>
2019-06-24 22:17:57 +0000

    created head.js and implemented function to find head of an array

--- w1/lotide/assertEqual.js
+++ w1/lotide/assertEqual.js
@@ -4,7 +4,12 @@ const assertEqual = function(actual, expected){
     } else {
         return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
     }
-}
+};
+
+// TEST CASES
 
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 1));
+console.log(assertEqual(1, "lighthouse"));
+console.log(assertEqual(true, 1));
+console.log(assertEqual(true, "hello"));
